233dc71b0166683f83cd5cd1e87b3760f70dccdf
Luca Scherzer <83914554+lucascherzer@users.noreply.github.com>
2023-04-26 01:11:58 +0200

    rename symbols, add documentation and remove old constants

--- src/exit_codes.rs
+++ src/exit_codes.rs
@@ -2,17 +2,25 @@
 
 
 pub enum DebugStatus {
-    DBG_BEINGEBUGGEDPEB,
-    DBG_ISDEBUGGERPRESENT,
-    DBG_NTGLOBALFLAGPEB,
-    DBG_CHECKREMOTEDEBUGGERPRESENT,
-    DBG_FINDWINDOW,
-    DBG_PROCESSFILENAME,
-    DBG_NONEFOUND
+    /// The PEB flag indicating the presence of a debugger is set. The presence of a debugger is highly likely
+    BeingDebuggedPeb,
+
+    /// A call to [IsDebuggerPresent](winapi::um::debugapi::IsDebuggerPresent) indicates the presence of a debugger
+    IsDebuggerPresent,
+
+    /// The [NtGlobalFlag](https://www.aldeid.com/wiki/PEB-Process-Environment-Block/NtGlobalFlag) of the PEB indicates the presence of a debugger
+    NtGlobalFlagPeb,
+
+    /// A call to [CheckRemoteDebuggerPresent](winapi::um::debugapi::CheckRemoteDebuggerPresent) indicates the presence of a remote debugger
+    RemoteDebuggerPresent,
+
+    /// An open GUI program's name matches a known debugger. This likely means that a debugger is present 
+    FoundOpenWindow,
+
+    /// A running process' name matches a known debugger. This likely means that a debugger is present
+    DebuggerProcessFilename,
+
+    /// No debugger was found
+    None
 }
-pub const DBG_BEINGEBUGGEDPEB: i32 = 0x1000;
-pub const DBG_ISDEBUGGERPRESENT: i32 = 0x1002;
-pub const DBG_NTGLOBALFLAGPEB: i32 = 0x1003;
-pub const DBG_CHECKREMOTEDEBUGGERPRESENT: i32 = 0x1001;
-pub const DBG_FINDWINDOW: i32 = 0x1005;
-pub const DBG_PROCESSFILENAME: i32 = 0x1009;
+
