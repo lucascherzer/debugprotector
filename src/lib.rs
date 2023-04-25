c2ac6c3e214953d1618910a8501355dca16d38bd
Luca Scherzer <83914554+lucascherzer@users.noreply.github.com>
2023-04-26 01:15:02 +0200

    add `debugger_present` function and change `detect` to use it while maintaining the original behavior of exiting

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
 mod antidbg;
 mod exit_codes;
 
+use crate::exit_codes::DebugStatus;
+
 pub fn detect() {
-    unsafe {
-        antidbg::adbg_is_debugger_present();
-        antidbg::adbg_being_debugged_peb();
-        antidbg::adbg_nt_global_flag_peb();
-        antidbg::adbg_check_remote_debugger_present();
-        antidbg::adbg_check_window_class_name();
-        antidbg::adbg_check_window_name();
-        antidbg::adbg_process_file_name();
+    if debugger_present() {
+        std::process::exit(0); // Exit code would need to be changed accordingly
+    }
+}
+
+/// Checks whether there is a debugger present
+/// # Returns
+/// * `true` if it found a debugger
+/// * `false` if it did **not** find a debugger
+pub fn debugger_present() -> bool {
+    let checks: [unsafe fn() -> DebugStatus; 7] = [
+            antidbg::adbg_is_debugger_present,
+            antidbg::adbg_being_debugged_peb,
+            antidbg::adbg_nt_global_flag_peb,
+            antidbg::adbg_check_remote_debugger_present,
+            antidbg::adbg_check_window_class_name,
+            antidbg::adbg_check_window_name,
+            antidbg::adbg_process_file_name
+        ];
+        unsafe {
+        for check in checks {
+            if let DebugStatus::None = check() {
+                continue
+            } else {
+                return true
+            }
+        }
+        false
     }
 }
