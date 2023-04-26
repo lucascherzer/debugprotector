31bbc75e7b8edc3987e75adab0d84c8463157f0a
Luca Scherzer <83914554+lucascherzer@users.noreply.github.com>
2023-04-26 14:29:38 +0200

    extract `CHECKS` into a global const

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,16 @@ mod exit_codes;
 
 use crate::exit_codes::DebugStatus;
 
+const CHECKS: [unsafe fn() -> DebugStatus; 7] = [
+    antidbg::adbg_is_debugger_present,
+    antidbg::adbg_being_debugged_peb,
+    antidbg::adbg_nt_global_flag_peb,
+    antidbg::adbg_check_remote_debugger_present,
+    antidbg::adbg_check_window_class_name,
+    antidbg::adbg_check_window_name,
+    antidbg::adbg_process_file_name
+];
+
 // Check for a debugger and exit if there is one
 pub fn detect() {
     if debugger_present() {
@@ -15,17 +25,8 @@ pub fn detect() {
 /// * `true` if it found a debugger
 /// * `false` if it did **not** find a debugger
 pub fn debugger_present() -> bool {
-    let checks: [unsafe fn() -> DebugStatus; 7] = [
-            antidbg::adbg_is_debugger_present,
-            antidbg::adbg_being_debugged_peb,
-            antidbg::adbg_nt_global_flag_peb,
-            antidbg::adbg_check_remote_debugger_present,
-            antidbg::adbg_check_window_class_name,
-            antidbg::adbg_check_window_name,
-            antidbg::adbg_process_file_name
-        ];
-        unsafe {
-        for check in checks {
+    unsafe {
+        for check in CHECKS {
             if let DebugStatus::None = check() {
                 continue
             } else {
